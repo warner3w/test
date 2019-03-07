0eac35f29107ced8fabd139ca35dbb4a1f53830d
warner3w <48308800+warner3w@users.noreply.github.com>
2019-03-07 14:12:35 +0800

    Add files via upload

---
+++ WebApplication1/WebApplication1/fileUpload.ashx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// fileUpload 的摘要说明
+    /// </summary>
+    public class fileUpload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        { 
+              String str = "{\"code\":";
+             str = str + "200,\"meessage\":\"success\",\"src\":";
+
+             str = str + "http://www.st-saas.com:8088/webclient/test.apk";
+                    
+         
+
+
+           context.Response.ContentType = "text/plain";
+           context.Response.Write(str);
+ 
+          
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
